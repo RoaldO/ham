b9ee0e53990fdd22e3187e4bcb6d325aec48ec7c
Roald Osinga <roald@shoppingminds.com>
2016-12-04 12:17:56 +0100

    set up static reasource loading for tiles

---
+++ CUSTOMIZATION.md
@@ -0,0 +1,4 @@
+The most important customization for the web interface is the possibility to add your own tile types.
+To do so, add an htl file with th e name <type>.html to the folder 'templaces/tiles' and add the static resources
+'<type>.css' and '<type>.js' to the folder 'static/tiles'. You have to do so, even if you dont need javascript or custom
+stylesheets. Just leave them empty in that case.
--- src/ham_server.py
+++ src/ham_server.py
@@ -1,13 +1,35 @@
 from flask import Flask, render_template
+from collections import namedtuple
+
+Tile = namedtuple('Tile', 'type title width height properties')
 
 app = Flask(__name__)
+app.config.APP_STRUCTURE = \
+    Tile(type='menu',
+        title="Hoofdmenu",
+        width=0,
+        height=0,
+        properties=dict(
+            tiles=[
+                Tile(type='menu', title='verlichting', width=1, height=1, properties=dict()),
+                Tile(type='menu', title='verwarming', width=1, height=1, properties=dict()),
+                Tile(type='switch', title='light above dining table', width=1, height=1, properties=dict()),
+            ])
+        )
 
 def render_tile(tile):
-    return "tile"
+    return render_template("tiles/{0}.html".format(tile.type))
 
 @app.route('/')
 def index():
-    return render_template('index.html', tiles=[], render_tile=render_tile)
+    tiles = app.config.APP_STRUCTURE.properties['tiles']
+    types = [tile.type for tile in tiles]
+    unique_types = set(types)
+    return render_template('index.html', render_tile=render_tile,
+                                         title=app.config.APP_STRUCTURE.title,
+                                         tiles=tiles,
+                                         types=unique_types,
+                           )
 
 
 if __name__ == '__main__':
